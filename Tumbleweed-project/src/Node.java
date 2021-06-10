92daa39cce59900cddf2d3c1c16019bfa94f6956
Spige123 <60225382+Spige123@users.noreply.github.com>
2021-06-10 17:36:20 +0300

    stuff

--- Tumbleweed-project/src/Node.java
+++ Tumbleweed-project/src/Node.java
@@ -1,2 +1,12 @@
 public interface Node {
+
+    public int funcaaaation (int nodeNummber,int infoToProces){
+        //check if nodeNumber is available
+        // if it is not available then sout(node error)
+        //if available then process and return infoProcessed
+
+        // does stuff with available nodes
+    }
+
+    public shoufleNodes(){}
 }
