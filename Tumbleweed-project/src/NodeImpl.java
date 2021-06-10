4f1db52d3999446f5fde73515f733c4d9b81f4f1
Spige123 <60225382+Spige123@users.noreply.github.com>
2021-06-10 19:18:13 +0300

    node implementation

---
+++ Tumbleweed-project/src/NodeImpl.java
@@ -0,0 +1,54 @@
+public class NodeImpl implements Node {
+
+    private boolean availability;
+    private boolean brokenness;
+    private boolean ProcessedDataAvailability;
+    private final int testValue = 0;
+    private int ticksToProcess = -1;
+    private int output = -1;
+    private int tickCounter=-1;
+    private double failureProbability = 0.01;
+
+
+
+    @Override
+    public boolean isAvailable(){
+        return availability;
+    }
+
+    /*@Override
+    public boolean isBroken() {
+        return brokenness;
+    }
+     */
+
+    @Override
+    public void process(int rawData,int n) throws Exception {
+        if(!brokenness && availability){
+            tickCounter = n;
+            if(rawData == testValue){
+                output =testValue;
+                ticksToProcess=0;
+            }else{
+                output = rawData%101;
+                ticksToProcess = (int)(java.lang.Math.random()*10)+2;
+            }
+        }
+        availability=false;
+        if(java.lang.Math.random()<failureProbability)brokenness=true;
+    }
+
+    @Override
+    public boolean isProcessedDataAvailable(int n) {
+        if(n<(ticksToProcess+tickCounter-1)||brokenness)ProcessedDataAvailability=false;
+        else ProcessedDataAvailability=true;
+        return ProcessedDataAvailability;
+    }
+
+    @Override
+    public int getProcessedData() {
+        if(brokenness)return (int)(java.lang.Math.random()*101);
+        if(ProcessedDataAvailability) return output;
+        return -1;
+    }
+}
--- Tumbleweed-project/src/Scheduler.java
+++ Tumbleweed-project/src/Scheduler.java
@@ -1,2 +1,6 @@
 public class Scheduler {
+
+    int n = 5 // number of nodes
+    Node[] network = new Node[n]; // where n is the nr of nodes in the network
+
 }
