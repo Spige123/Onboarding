847812bf787425eade6fbb83dd0007051210f987
Andrei <a.c.tociu@student.tudelft.nl>
2021-06-13 19:10:54 +0200

    simplified node implementation

--- Tumbleweed-project/src/Node.java
+++ Tumbleweed-project/src/Node.java
@@ -9,22 +9,16 @@ public interface Node {
     boolean isAvailable();
 
     /**
-     * states if the node is broken/functional or not
-     * @return true if node is broken, false otherwise
-     */
-    //boolean isBroken();
-
-    /**
+     * sends data to the node for processing
      * @param rawData the raw data to process
-     * @throws Exception exception in case processing fails
      */
-    void process(int rawData, int n) throws Exception;
+    void process(int rawData);
 
     /**
      * checks if the node has finished processing data
      * @return true if information has been processed, false otherwise
      */
-    boolean isProcessedDataAvailable(int n);
+    boolean isProcessedDataAvailable();
 
     /**
      * retrieves the processed data from the node
--- Tumbleweed-project/src/NodeImpl.java
+++ Tumbleweed-project/src/NodeImpl.java
@@ -1,57 +1,56 @@
 public class NodeImpl implements Node {
 
-    private boolean availability;
-    private boolean brokenness;
-    private boolean ProcessedDataAvailability;
-    private final int testValue = 0;
-    private int ticksToProcess = -1;
-    private int output = -1;
-    private int tickCounter=-1;
-    private double failureProbability = 0.01;
+    private boolean available;
+    private final int testValue = -1;
+    private int output;
+    private int tickCounter;
 
 
-
+    /**
+     * indicates if the node is available to process new data
+     * @return true if available, false otherwise
+     */
     @Override
     public boolean isAvailable(){
-        return availability;
+        return available;
     }
 
-    /*@Override
-    public boolean isBroken() {
-        return brokenness;
-    }
+    /**
+     * sends raw data to the node to process
+     * @param rawData the raw data to process
      */
-
     @Override
-    public void process(int rawData,int n) throws Exception {
-        if(!brokenness && availability){
-            tickCounter = n;
-            if(rawData == testValue){
-                output =testValue;
-                ticksToProcess=0;
-            }else{
-                output = rawData%101;
-                ticksToProcess = (int)(java.lang.Math.random()*10)+2;
-            }
+    public void process(int rawData) {
+        available = false;
+        if (rawData == testValue) {
+            output = testValue;
+            tickCounter = 0;
+        } else {
+            tickCounter = (int) Math.floor(Math.random() * (6 - 1) + 1); // return random Integer in range [1, 5]ù
+            output = Integer.valueOf(rawData).hashCode();
         }
-        availability=false;
-        if(java.lang.Math.random()<failureProbability)brokenness=true;
     }
 
+    /**
+     * shows if the node has finished processing the data
+     * @return true if available, false otherwise
+     */
     @Override
-    public boolean isProcessedDataAvailable(int n) {
-        if(n<(ticksToProcess+tickCounter-1)||brokenness)ProcessedDataAvailability=false;
-        else ProcessedDataAvailability=true;
-        return ProcessedDataAvailability;
+    public boolean isProcessedDataAvailable() {
+        if (tickCounter == 0) {
+            return true;
+        }
+        tickCounter--;
+        return false;
     }
 
+    /**
+     * gets the latest processed data from the node
+     * @return processed data package
+     */
     @Override
     public int getProcessedData() {
-        if(brokenness)return (int)(java.lang.Math.random()*101);
-        if(ProcessedDataAvailability) {
-            availability =true;
-            return output;
-        }
-        return -1;
+        available = true;
+        return output;
     }
 }
